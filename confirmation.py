c90a8059db706e6901ac0037ad1d2f71467b0f02
Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2024-05-18 01:22:08 +0200

    Add files via upload

---
+++ confirmation.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Display the confirmation message
+st.title('Inscription confirmée')
+st.success('Merci pour votre inscription. Votre inscription a été confirmée avec succès.')
+
